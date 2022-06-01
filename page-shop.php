1045b1caa64a6080137ae811ccb18c74b45be1fe
Laura kapper <laurakapper@gmail.com>
2022-06-01 16:14:20 +0200

    nu prøver jeg

--- page-shop.php
+++ page-shop.php
@@ -113,8 +113,33 @@ let klon = temp.cloneNode(true).content;
 
 }
 
-const btn = document.querySelector("#mobil_filtering")
+const btn = document.querySelector("#mobil-filter-knap")
 
+
+
+const kategorier = document.querySelector("#filter");
+// const navbarLinks = document.querySelector(".navbar-links");
+
+// Der lyttes på burger menuen, om der bliver klikket
+// Hvis der bliver klikket så skal den toggle klassen "active" på navigationen altså "åbne/display flex"
+// Så skal burger menuen gemmes væk og vise krydset. (Gøres også med toggle)
+
+btn.addEventListener("click", () => {
+	kategorier.classList.toggle("vis_ikke");
+//  lukbtn.classList.toggle("hidden");
+//   btn.classList.toggle("hidden");
+});
+
+// Så lytter vi efter klik på krydset, og hvis der klikkes på den skal det omvendte
+// af det ovenståendende ske.
+
+// lukbtn.addEventListener("click", () => {
+//   navbarLinks.classList.toggle("active");
+//   lukbtn.classList.toggle("hidden");
+//   btn.classList.toggle("hidden");
+// });
+
+// window.addEventListener("load", sidenVises);
 
 </script>
 
