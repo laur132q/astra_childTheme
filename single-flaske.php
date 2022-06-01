1b9e5c656db42c8dac46b26805c9818cfeb7a622
Your Name <youremail@yourdomain.comAndreaandr52j9@stud.kea.dk>
2022-06-01 12:57:00 +0200

    d

--- single-flaske.php
+++ single-flaske.php
@@ -8,7 +8,7 @@ get_header();
 ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<main id="singleview_main" class="site-main">
 
 
       <article id="singleView-slik">
